52f4db98f5fcee89df3ad8675e9902573eb57911
Lucas-BRT <93885104+Lucas-BRT@users.noreply.github.com>
2023-05-17 20:26:30 -0300

    adicionado Regex para porta
    Melhorada a checagem dos entry points de conexão.
    
    Adicionada uma checagem usando Regex na port.
    
    Adicionada uma protoclasse para futuros warnings, assim informando onde está o erro nos dados do entry point.
    
    apagado um console.log isolado desnecessário.

--- src/form.jsx
+++ src/form.jsx
@@ -5,7 +5,6 @@ import AppContext from './context/AppContext';
 
 function Form() {
   const {address, setAddress, disable, setDisable} = useContext(AppContext)
-  console.log(disable)
   const handleInput = ({target}) => {
     const { value, name } = target
     setAddress((prevState) => ({
@@ -14,14 +13,65 @@ function Form() {
     }))
   }
 
-  useEffect(() => {
-    const regex = /^(\d{1,3}\.){3}\d{1,3}$/;
-    if (!regex.test(address.ip)) {
-      setDisable(true)
-    } else {
-      setDisable(false)
+  class warningEntry {
+    constructor(identifier) {
+      this.identifier = identifier
     }
-  }, [address.ip, setDisable])
+
+    raiseRedWarning() {
+      // tornar o entry point em vermelho para chamar a atenção do usuário para aquele campo
+      // informando que é um erro que impede a conecção
+    }
+
+    raiseYellowWarning() {
+      // tornar o entry point em amarelo para chamar a atenção do usuário para aquele campo
+      // nesse caso é para ocasiões como deixar a porta em branco
+      // apesar de já haver uma checagem para garantir a execução, pessoas menos atentas
+      // podem acabar não lembrando da porta caso a porta 0 não seja a da conexão
+    }
+  }
+
+  class ValidateConection {
+    constructor(ip,port) {
+      this.ip = ip;
+      this.port = port;
+      if (port == "") {
+        this.port = 0
+      }
+    }
+    
+    validateConection() {
+      return (this.validateIP() && this.validatePort())
+    }
+
+    validateIP() {
+      const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+      return ipRegex.test(this.ip)
+    }
+
+    validatePort() {
+      const portRegex = /^(0|6553[0-5]|655[0-2]\d|65[0-4]\d{2}|6[0-4]\d{3}|[1-5]\d{4}|\d{1,4})$/;
+      return portRegex.test(this.port)
+    }
+  }
+
+  useEffect(() => {
+    const conection = new ValidateConection(address.ip,address.port)
+    const allowConection = conection.validateConection()
+
+    if (allowConection) {
+      setDisable(false)
+    } else {
+      if (!conection.validateIP()) {
+        setDisable(true)
+        // warning red
+      } else {
+        setDisable(true)
+        // warning yellow
+      }
+    }
+  
+  }, [address.ip,address.port,setDisable])
 
   return (
     <div className="view">
