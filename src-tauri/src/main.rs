c48d672d716c200de53ddfd67664b24517edb9f4
Lucas-BRT <93885104+Lucas-BRT@users.noreply.github.com>
2023-05-30 21:52:17 -0300

    versão bugada para Janderson

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,41 @@
 // Adds suport for async traits
 #![feature(async_fn_in_trait)]
 
+use kenkrusty_api::{Sound, Track};
 mod kenkrusty_api;
+mod Falcon;
+ 
+
+use std::thread;
+
+
+#[tauri::command]
+async fn is_kenku_remote_avaliable(ip: String, port: String) -> bool {
+    kenkrusty_api::is_kenku_remote_avaliable(&ip, &port).await
+}
+
+#[tauri::command]
+async fn get_sounds(ip: String, port: String) -> Vec<Sound> {
+    let board = kenkrusty_api::MediaBoard::new(ip, port).await;
+    board.get_sounds()
+}
+
+#[tauri::command]
+async fn get_tracks(ip: String, port: String) -> Vec<Track> {
+    let board = kenkrusty_api::MediaBoard::new(ip, port).await;
+    board.get_tracks()
+}
+
 
 #[tokio::main]
 async fn main() {
 
+    let macaco = thread::spawn(|| { 
+        Falcon::launch();
+    });
+    
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![is_kenku_remote_avaliable,get_sounds,get_tracks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
---
+++ src/pages/Soundboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/SoundBoard.css'
+
+export default function Soundboard() {
+  return (
+    <div className='Page'>
+      <div className='Playlists-Sounds'>
+        <div className='Playlist'>
+          <p>heelo</p>
+        </div>
+        <div className='Soundbord'>
+          <p>heelo</p>
+        </div>
+      </div>
+      <div className='CustomPlay' >
+        <button>+</button>
+      </div>
+    </div>
+  )
+}
